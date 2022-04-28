aeec087c222d00a6d525cf3cc63c00efec10cc80
Hossam Adel <hossamadel23895@gmail.com>
2022-04-28 17:02:40 +0200

    added admin lte package

---
+++ resources/views/layouts/menu.blade.php
@@ -0,0 +1,11 @@
+<!-- need to remove -->
+<li class="nav-item">
+    <a href="{{ route('home') }}" class="nav-link {{ Request::is('home') ? 'active' : '' }}">
+        <i class="nav-icon fas fa-home"></i>
+        <p>Home</p>
+    </a>
+    <a href="/dashboard" class="nav-link {{ Request::is('dashboard') ? 'active' : '' }}">
+        <i class="nav-icon fas fa-dashboard"></i>
+        <p>City Managers</p>
+    </a>
+</li>
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Auth::routes(['register' => false]);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
